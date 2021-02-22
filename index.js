112c838dc30b2481863bfdd443b65c97b9ec4464
Halık Ali TUNÇ <tunchalikali@gmail.com>
2021-02-22 09:21:59 +0300

    quotes app general template

---
+++ index.js
@@ -0,0 +1,10 @@
+const baseUrlForQuotes = "https://goquotes-api.herokuapp.com/api/v1/random?count=1" ; 
+const fetchQuotes = () => {
+    fetch(baseUrlForQuotes).then(Response => Response.json()).then(data => {
+        document.getElementById("quote").innerHTML = data.quotes[0].text , 
+        document.getElementById("author").innerHTML = `Yazar : ${data.quotes[0].author}`,
+        document.getElementById("tag").innerHTML = `Kategori : ${data.quotes[0].tag}`
+    })
+}
+
+window.addEventListener('load',fetchQuotes());
